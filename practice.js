e7ef5daa87bcb29d9818566a5817e9a9e9c8bdf4
arnoldarchaga <155139893+arnoldarchaga@users.noreply.github.com>
2024-06-04 23:10:14 -0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,19 @@
+var firstName = "Arnold";
+var lastName = "Archaga";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+var age = 23;
+if (age > 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
+for (var i = 0; i < 5; i++) {
+    console.log("I love coding");
+}
+// Calculate my birth year
+var currentYear = 2024;
+var age = 23;
+var birthYear = currentYear - age;
+console.log(birthYear);
+// Calculate my age
